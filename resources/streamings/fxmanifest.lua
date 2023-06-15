8e991222c90b068e1a62f3abf1727886f4ab49ad
Diegohbss <107653207+Diegohbss@users.noreply.github.com>
2023-06-15 02:46:09 +0200

    init

---
+++ resources/streamings/fxmanifest.lua
@@ -0,0 +1,119 @@
+fx_version 'adamant'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+data_file 'TIMECYCLEMOD_FILE' 'gabz_mrpd_timecycle.xml'
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+files {
+	'gabz_mrpd_timecycle.xml',
+	'interiorproxies.meta'
+}
+
+client_script {
+    "gabz_mrpd_entitysets.lua"
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/discoverybuilder.ytyp'
+
+file 'gabz_timecycle_mods_1.xml'
+
+data_file 'TIMECYCLEMOD_FILE' 'gabz_timecycle_mods_1.xml'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/builderdefaddons.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/altercity_sign1.ytyp'
+
+client_scripts({
+	'client/main.lua'
+})
+
+data_file 'DLC_ITYP_REQUEST' 'stream/vw_ch3_additions.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/vw_ch3_additions_strm.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/hei_ch3_lod.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/hei_cityhills_03_metadata_001.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/hei_cityhills_03_metadata_001_strm.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_accs.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_accs_01.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_animated_walls.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_cabinets.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_cards.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_casino.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_casinochips.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_casino_art_01.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_casino_art_02.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/vw_prop_vw_casino_art_03.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_casino_art_04.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_casino_art_05.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_collectibles.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_disposal.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_door_break.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_machinery.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_screens.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_storage.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_tables.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'vw_prop_vw_turntable.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/fv_props.ytyp'
+
+files {
+	'mp_m_freemode_01_mp_m_clothing_one.meta',
+	'mp_f_freemode_01_mp_f_clothing_one.meta'
+}
+  
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_clothing_one.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_clothing_one.meta'
+
+files {
+	'mp_m_freemode_01_mp_m_cloting_pack_two.meta',
+	'mp_f_freemode_01_mp_f_cloting_pack_two.meta'
+}
+  
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_cloting_pack_two.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_cloting_pack_two.meta'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/vesp_props.ytyp'
+
+data_file ('DLC_ITYP_REQUEST') ('stream/int_cayo_props.ytyp')
+data_file ('DLC_ITYP_REQUEST') ('stream/med/int_cayo_med_props.ytyp')
+
+data_file "DLC_ITYP_REQUEST" "stream/props/prison_props.ytyp"
+
+data_file 'DLC_ITYP_REQUEST' 'stream/ydr/gabz_lost_props.ytyp'
+
+files {
+	'peds.meta'
+}
+data_file 'PED_METADATA_FILE' 'peds.meta'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/ffxv_galdinquay.ytyp'
+
+--Planque
+data_file 'DLC_ITYP_REQUEST' 'stream/[mappings]/planque/bkr_biker_dlc_int_01.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/[mappings]/planque/bkr_biker_dlc_int_02.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/[mappings]/planque/tr_dlc_int_01_tr.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/[mappings]/planque/tr_dlc_int_01_tr.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/[mappings]/planque/xs_x18dlc_int_vip.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/[mappings]/planque/int_corporate.ytyp'
+
+-- Altruist
+data_file 'DLC_ITYP_REQUEST' 'stream/[mappings]/dolu_altruists/def_props.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/[mappings]/dolu_altruists/int_fhouse01.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/[mappings]/dolu_altruists/int_fhouse02.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/[mappings]/dolu_altruists/int_fuzzbarn.ytyp'
+data_file 'TIMECYCLEMOD_FILE' 'dolu_timecycle.xml'
+
+files {"dolu_timecycle.xml"}
+
+-- Cardealer
+data_file 'DLC_ITYP_REQUEST' 'stream/[mappings]/cardealer/patoche_cyber_list.ytyp'
+
+-- Lux
+data_file('DLC_ITYP_REQUEST')('stream/[mappings]/molo_lux/sf_dlc_int_01_sec.ytyp')
+
+--☁️ AeroDefence 🚧
+client_script '@AeroDefence/client/c_antistop.lua'
+
+client_script '@AeroDefence/files/protect.lua'
